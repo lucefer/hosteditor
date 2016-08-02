df7041f86c5689c7fd852b6c3f7f546da4e84ed3
lucefer <fanqi_danding@126.com>
2016-08-02 18:20:07 +0800

    整理目录，修改命令

--- hostEdit.js
+++ hostEdit.js
@@ -1,40 +1,21 @@
-var os = require('os');
-var fs = require('fs');
+
+
+var core=require('./hostOper/core');
 
 var log = console.log;
 
-function getHost() {
-    var platform = os.platform();
-    console.log(platform);
-    platform = platform.indexOf("win32") > -1 ? "windows" : platform.indexOf("linux") > -1 ? "linux" : "other";
-    var hostPath = "C:\\windows\\system32\\drivers\\etc\\hosts";
-    switch (platform) {
-        case "linux":
-            hostPath = "/etc/hosts";
-
-    }
-    return hostPath;
+function modifyHost(ip,domain){
+    log(core.modifyHost(ip,domain));
 }
-
-function modifyHost(ip, domain) {
-    var host = getHost();
-    if(!fs.existsSync(host)){
-        log("未找到host文件");
-        return;
-    }
-    var content = fs.readFileSync(host, "utf-8");
-    var reg = new RegExp("#?\\s*" + ip + "\\s*" + domain + "\\s*\S*\\r\\n", "g");
-    var arr;
-    while ((arr = reg.exec(content)) !== null) {  
-        if ((arr[0].charAt(0)) === '#') {
-            content = (content.slice(0, arr.index) + content.slice(arr.index + 1, content.length));
-            fs.writeFile(host, content);
-            return;
-            //console.log(content[arr.index]);
-        } else {
-            return;
-        }
-    }
-    fs.appendFile(host, ip + "\t" + domain + "\r\n");
+function listHosts(){
+    var arr=core.getHosts();
+    log('当前启用的host:');
+    log(arr.reduce(function(preValue,currValue){
+        if(Object.prototype.toString.call(preValue)=='[object Object]')
+            {
+                preValue="\t"+preValue.ip+"\t"+preValue.domain+"\n";
+            }
+        return preValue+"\t"+currValue.ip+"\t"+currValue.domain+"\n";
+    }))
 }
-module.exports=modifyHost
+module.exports={modifyHost:modifyHost,listHosts:listHosts};
--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
-var hostEdit=require('./hostEdit');
+var hostEdit = require('./hostEdit');
 
-
-var ipExp=/((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))/,
-	domainExp= /^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$/,
-	ip=process.argv[2],
-	domain=process.argv[3],
-	err="";
-	
-if(!ipExp.test(ip)){
-	err+="IP地址不合法\r\n";
-	
+var ipExp = /((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))/,
+    domainExp = /^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$/,
+    ip = process.argv[2],
+    domain = process.argv[3],
+    err = "";
+switch (ip) {
+    case 'ls':
+        hostEdit.listHosts();
+        break;
+    default:
+        if (!ipExp.test(ip)) {
+            err += "IP地址不合法\r\n";
+        }
+        if (!domainExp.test(domain)) {
+            err += "域名不合法";
+        }
+        if ("" != err) {
+            console.log(err);
+            return;
+        }
+        hostEdit.modifyHost(ip, domain);
+        break;
 }
-if(!domainExp.test(domain)){
-	err+="域名不合法";
-
-}
-if(""!=err){
-	console.log(err);
-	return;
-}
-hostEdit(ip,domain); 
-
